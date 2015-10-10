b600fbb6032a027e8e83b9443252f38213c88cf5
Nathan LaFreniere <quitlahok@gmail.com>
2015-10-10 11:47:49 -0700

    total rewrite, version 4.0.0

--- README.md
+++ README.md
@@ -1,166 +1,63 @@
 ## wadofgum [![Build Status](https://travis-ci.org/nlf/wadofgum.svg)](https://travis-ci.org/nlf/wadofgum)
 
-A super minimal modeling library built around [joi](https://github.com/hapijs/joi).
-
-Joi is used for all schema definition and validation, all this library provides is a way of using predefined schemas in a prototypical manner as well as means of simply extending these prototypes.
+A super minimal and highly extensible modeling library.
 
 ### Usage
 
-The exported method is a constructor used to create a model factory. It accepts a single object parameter, and requires `type` and `schema` properties. For example:
+The basic form of *wadofgum* is essentially just an object with a few additions to simplify using it as the base for a more robust modeling system.
 
-```javascript
-var Factory = require('wadofgum');
+```js
+const Wadofgum = require('wadofgum');
 
-var User = new Factory({
-    type: 'user',
-    schema: {
-        id: Joi.number(),
-        firstName: Joi.string(),
-        lastName: Joi.string()
+let model = new Wadofgum();
+```
+
+#### Capabilities
+
+The base class and all classes derived from it contain a `capabilities` property, which is a `Set` containing the names of capabilities that have been added to your class. By default, this set is empty. It is intended to be added to when loading a mixin.
+
+For example:
+
+```js
+const Wadofgum = require('wadofgum');
+Wadofgum.capabilities.add('my_cap');
+```
+
+#### Metadata
+
+Additionally the base class exports a `meta` property which can be used to store metadata about your class in a `WeakSet`. The meta object contains three methods, `set`, `get`, and `has`.
+
+```js
+const Wadofgum = require('wadofgum');
+
+Wadofgum.meta.has('key'); // false
+Wadofgum.meta.get('key'); // undefined
+
+Wadofgum.meta.set('key', 'value');
+Wadofgum.meta.has('key'); // true
+Wadofgum.meta.get('key'); // 'value'
+```
+
+### Mixins
+
+A mixin is simply a function that accepts a base class as a parameter, performs some manipulation of it, and returns a new class:
+
+```js
+const Wadofgum = require('wadofgum');
+
+let myMixin = function (baseClass) {
+    class myModel extends baseClass {
+        static myMethod() {
+            console.log('called myMethod');
+        };
     }
-});
-```
 
-The schema property can be passed as an object literal, as above, or as a joi schema. The following is equivalent to the first example:
+    myModel.capabilities.add('myMethod');
 
-```javascript
-var User = new Factory({
-    type: 'user',
-    schema: Joi.object({
-        id: Joi.number(),
-        firstName: Joi.string(),
-        lastName: Joi.string()
-    })
-});
-```
-
-Also supported is an optional `plugins` key, which can be used to load plugins on the factory at creation time.
-
-```javascript
-var User = new Factory({
-    type: 'user',
-    schema: {
-        id: Joi.number(),
-        firstName: Joi.string(),
-        lastName: Joi.string()
-    },
-    plugins: [PluginOne, PluginTwo]
-});
-```
-
-This factory can then be used to create model instances. These instances are validated on creation which allows joi to perform type coercion immediately. Errors in the initial validation are simply ignored.
-
-```javascript
-var User = new Factory({
-    type: 'user',
-    schema: {
-        id: Joi.number(),
-        firstName: Joi.string(),
-        lastName: Joi.string()
-    }
-});
-
-var user = new User({ id: '40', firstName: 'Jane', lastName: 'Doe' });
-user.id === 40 // true
-```
-
-Note that model instances will automatically have a reference to their factory as the property `factory`.
-
-```javascript
-var user = new User();
-user.factory === User; // true
-```
-
-To attach methods to the factory, simply assign them.
-
-```javascript
-User.find = function (query, callback) {
+    return myModel;
 };
+
+class Model extends Wadofgum.mixin(myMixin) {};
+Model.myMethod(); // 'called myMethod'
+Model.capabilities.has('myMethod'); // true
 ```
-
-To attach methods to a model instance, assign them to the factory's prototype.
-
-```javascript
-User.prototype.save = function (callback) {
-};
-```
-
-Note that later assignments to the factory's `schema` property will pass through a setter method that will concat the new schema with the existing one, as in:
-
-```javascript
-var User = new Factory({
-    type: 'user',
-    schema: {
-        name: Joi.string(),
-        age: Joi.number().integer()
-    }
-});
-
-User.schema = Joi.object({ favoriteColor: Joi.string() });
-User.schema = { luckyNumber: Joi.number().integer() };
-// User.schema will now have keys for 'name', 'age', 'favoriteColor', and 'luckyNumber'
-```
-
-#### `validate([next])`
-
-Validation can also be triggered manually, by calling the `validate()` method on a model instance. This method accepts a callback that will receive any errors. Note that validating a model mutates properties in the same way as when instantiating a new model.
-
-```javascript
-user.id = '100';
-user.validate(function (err) {
-    user.id === 100 // true
-});
-```
-
-#### `register([plugins])`
-
-wadofgum model factories are easily extended using plugins. A plugin can be passed as either a function directly, or as an object with `register` and `options` properties.
-
-```javascript
-User.register(SomePlugin, SecondPlugin);
-
-User.register({
-    register: AnotherPlugin,
-    options: {
-        someOption: true
-    }
-});
-```
-
-#### `extend(obj)`
-
-The `extend` method is used to extend the current factory with another factory. This is useful for creating a base factory that contains common plugins for your other factories to inherit. The schema from the two factories will also be merged.
-
-```javascript
-var Base = new Factory({
-    type: '_base',
-    schema: {
-        id: Joi.string()
-    },
-    plugins: [SomePlugin, AnotherPlugin]
-});
-
-User.extend(Base); // User will now have SomePlugin and AnotherPlugin loaded, as well as an 'id' key available in the schema
-```
-
-#### events
-
-The model factory and model instances each have event emitters. These event emitters are provided by [spit](https://github.com/nlf/spit) and are asynchronous. See the README in that repo for more details.
-
-To add a listener for factory events, you may simply use `User.on(event, fn)`. Events can also be emitted on a model factory by calling `User.emit(event, params)`.
-
-Model factories, by default, only emit one event `create`. The parameter passed to this event is the model instance that was just created.
-
-To add a listener for model instance events, you may either use `user.on(event, fn)` where `user` is an instance of a model, or you may use `User.prototype.on(event, fn)` where `User` is a model factory.
-
-Model instances, by default, emit two events `preValidate` and `postValidate`. The parameter passed to both is a reference to the model instance.
-
-Events can only be emitted on a model instance if you have a reference to that instance, in which case you may simply use `user.emit(event, params)`.
-
-### Plugins
-
-A plugin is a method that receives two parameters, `factory` and `options`.
-
-The `options` parameter is passed directly from the call to `register()`. If no options are given, this parameter will be an empty object `{}`.
-
-The `factory` parameter is a reference to the factory object this plugin is being registered on. All the above methods are available.
--- test/index.js
+++ test/index.js
@@ -1,698 +1,107 @@
-var Factory = require('..');
-var Joi = require('joi');
+'use strict';
 
-var lab = exports.lab = require('lab').script();
-var expect = require('code').expect;
+let Model = require('..');
 
-lab.test('can create a model definition', function (done) {
+let lab = exports.lab = require('lab').script();
+let expect = require('code').expect;
+let it = lab.test;
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required()
-        }
-    });
+it('can create a model instance', function (done) {
 
-    expect(User).to.exist();
-    done();
-});
-
-lab.test('throws when bad options are supplied', function (done) {
-
-    expect(function () {
-
-        var User = new Factory();
-    }).to.throw();
-
-    expect(function () {
-
-        var User = new Factory({});
-    }).to.throw();
-
-    expect(function () {
-
-        var User = new Factory({ type: 'test' });
-    }).to.throw();
-
-    expect(function () {
-
-        var User = new Factory({ schema: {} });
-    }).to.throw();
-
-    expect(function () {
-
-        var User = new Factory({ plugins: [] });
-    }).to.throw();
-
-    done();
-});
-
-lab.test('can create a model instance', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: Joi.object().keys({
-            name: Joi.string().required(),
-            age: Joi.number().integer()
-        })
-    });
-
-    var user = new User({ name: 'test', age: '30' });
+    class User extends Model {};
+    let user = new User({ name: 'test', age: '30' });
     expect(user).to.exist();
     expect(user.name).to.equal('test');
     expect(user.age).to.equal('30');
     done();
 });
 
-lab.test('sets default values after validating', function (done) {
+it('can create an instance with no fields', function (done) {
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User();
-    user.validate().then(function () {
-
-        expect(user).to.exist();
-        expect(user.age).to.equal(20);
-
-        done();
-    });
+    class User extends Model {};
+    let user = new User();
+    expect(Object.keys(user).length).to.equal(0);
+    done();
 });
 
-lab.test('can validate', function (done) {
+it('can set metadata on a class', function (done) {
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User({ name: 'test', age: 30 });
-    user.validate().then(function () {
-
-        expect(user.name).to.equal('test');
-        expect(user.age).to.equal(30);
-        done();
-    });
+    class User extends Model {};
+    User.meta.set('key', 'value');
+    expect(User.meta.get('key')).to.equal('value');
+    User.meta.set('object', { some: 'thing' });
+    expect(User.meta.get('object')).to.deep.equal({ some: 'thing' });
+    done();
 });
 
-lab.test('calling validate converts values', function (done) {
+it('can see if a key exists in metadata', function (done) {
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User({ name: 'test', age: '30' });
-    user.validate().then(function () {
-
-        expect(user.name).to.equal('test');
-        expect(user.age).to.equal(30);
-        done();
-    });
+    class User extends Model {};
+    expect(User.meta.has('key')).to.equal(false);
+    User.meta.set('key', 'value');
+    expect(User.meta.has('key')).to.equal(true);
+    done();
 });
 
-lab.test('calling validate reports errors', function (done) {
+it('returns undefined when getting metadata that has not been set', function (done) {
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User({ age: '30' });
-    user.validate().catch(function (err) {
-
-        expect(err).to.exist();
-        expect(err.message).to.contain('"name" is required');
-        expect(user.age).to.equal(30);
-        done();
-    });
+    class User extends Model {};
+    expect(User.meta.get('test')).to.not.exist();
+    done();
 });
 
-lab.test('calling validate converts valid keys to the correct type', function (done) {
+it('can extend a class with a mixin', function (done) {
 
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            favorites: {
-                number: Joi.number().integer()
-            },
-            extras: {
-                likesMelon: Joi.boolean()
-            },
-            age: Joi.number().integer().default(20)
-        }
-    });
+    let mixin = function (baseClass) {
 
-    var user = new User({ name: 'test', age: '30', favorites: { number: '20' } });
-    user.validate().then(function () {
+        class SubModel extends baseClass {
 
-        expect(user.age).to.equal(30);
-        expect(user.favorites.number).to.equal(20);
-        done();
-    });
-});
+            static extended () {
 
-lab.test('calling validate does not convert keys which contain errors', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            favorites: {
-                number: Joi.number().integer()
-            },
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User({ name: 'test', age: 'test', favorites: { number: 'twenty' } });
-    user.validate().catch(function () {
-
-        expect(user.favorites.number).to.equal('twenty');
-        expect(user.age).to.equal('test');
-        done();
-    });
-});
-
-lab.test('calling validate reports multiple errors', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var user = new User({ age: 'test' });
-    user.validate().catch(function (err) {
-
-        expect(err).to.exist();
-        expect(err.message).to.contain('"name" is required');
-        expect(err.message).to.contain('"age" must be a number');
-        expect(user.age).to.equal('test');
-        done();
-    });
-});
-
-lab.test('calling validate removes renamed keys', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: Joi.object({
-            name: Joi.string(),
-            age: Joi.number().integer()
-        }).rename('_age', 'age')
-    });
-
-    var user = new User({ _age: 30 });
-    user.validate()
-        .then(function () {
-
-            expect(user.age).to.equal(30);
-            expect(user._age).to.not.exist();
-            done();
-        })
-        .catch(done);
-});
-
-lab.test('calling validate does not remove aliased renamed keys', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: Joi.object({
-            name: Joi.string(),
-            age: Joi.number().integer()
-        }).rename('_age', 'age', { alias: true })
-    });
-
-    var user = new User({ _age: 30 });
-    user.validate()
-        .then(function () {
-
-            expect(user.age).to.equal(30);
-            expect(user._age).to.equal(30);
-            done();
-        })
-        .catch(done);
-});
-
-lab.test('can use a plugin', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var initCalled = false;
-
-    var Plugin = function (model, options) {
-
-        expect(model).to.exist();
-        expect(model.type).to.equal('user');
-        expect(model.schema.isJoi).to.equal(true);
-        expect(initCalled).to.equal(false);
-        initCalled = true;
-
-        model.prototype.test = function () {
-
-            return this.name;
+                return true;
+            };
         };
 
-        model.test = function () {
+        return SubModel;
+    };
 
-            return true;
+    class User extends Model.mixin(mixin) {};
+    expect(User).to.exist();
+    expect(User.extended()).to.equal(true);
+    done();
+});
+
+it('can extend a class with multiple mixins', function (done) {
+
+    let mixin = function (baseClass) {
+
+        class SubModel extends baseClass {
+
+            static extended () {
+
+                return true;
+            };
         };
+
+        return SubModel;
     };
 
-    User.register({
-        register: Plugin
-    });
+    let mixin2 = function (baseClass) {
 
-    expect(initCalled).to.equal(true);
+        class SubModel extends baseClass {
 
-    var user = new User({ name: 'test' });
-    expect(user.test).to.exist();
-    expect(user.test()).to.equal('test');
+            static extendedAgain () {
 
-    expect(User.test).to.exist();
-    expect(User.test()).to.equal(true);
-    done();
-});
-
-lab.test('can load multiple plugins at once', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model, options) {
-
-        model.test = function () {
-
-            return true;
-        };
-    };
-
-    var PluginTwo = function (model, options) {
-
-        model.twice = true;
-    };
-
-    User.register([Plugin, PluginTwo]);
-    expect(User.test()).to.equal(true);
-    expect(User.twice).to.equal(true);
-    done();
-});
-
-lab.test('can load a plugin with a deep register property', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-    };
-
-    User.register({
-        register: {
-            register: Plugin
-        }
-    });
-
-    done();
-});
-
-lab.test('throws when trying to use an invalid plugin', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    expect(function () {
-
-        User.register({});
-    }).to.throw('Invalid plugin');
-
-    expect(function () {
-
-        User.register('broken');
-    }).to.throw('Invalid plugin');
-
-    done();
-});
-
-lab.test('can extend a model schema with a plugin', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-        expect(model.schema).to.exist();
-        expect(model.schema.isJoi).to.equal(true);
-
-        model.schema = Joi.object({
-            id: Joi.string().default('some_id')
-        });
-    };
-
-    User.register(Plugin);
-
-    var user = new User({ name: 'test' });
-    user.validate().then(function () {
-
-        expect(user.id).to.equal('some_id');
-        done();
-    });
-});
-
-lab.test('can load plugins during instantiation', function (done) {
-
-    var Plugin = function (model) {
-
-        model.test = 'object';
-    };
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string()
-        },
-        plugins: [Plugin]
-    });
-
-    var user = new User({ name: 'test' });
-    expect(user.name).to.equal('test');
-    expect(User.test).to.equal('object');
-    done();
-});
-
-lab.test('can extend a model with another model', function (done) {
-
-    var execCount = 0;
-
-    var Plugin = function (model) {
-
-        model.test = 'object';
-        ++execCount;
-    };
-
-    var Base = new Factory({
-        type: 'base',
-        schema: {
-            id: Joi.string()
-        },
-        plugins: [Plugin]
-    });
-
-    expect(execCount).to.equal(1);
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string()
-        }
-    });
-
-    User.extend(Base);
-    expect(execCount).to.equal(2);
-    expect(User.type).to.equal('user');
-    expect(User.test).to.equal('object');
-    expect(User.schema.describe()).to.deep.equal({
-        type: 'object',
-        children: {
-            name: {
-                type: 'string',
-                invalids: ['']
-            },
-            id: {
-                type: 'string',
-                invalids: ['']
+                return true;
             }
-        }
-    });
+        };
+
+        return SubModel;
+    };
+
+    class User extends Model.mixin(mixin, mixin2) {};
+    expect(User).to.exist();
+    expect(User.extended()).to.equal(true);
+    expect(User.extendedAgain()).to.equal(true);
     done();
-});
-
-lab.test('throws an error when attempting to extend a factory with an invalid factory', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string()
-        }
-    });
-
-    expect(function () {
-
-        User.extend();
-    }).to.throw('Invalid factory');
-
-    expect(function () {
-
-        User.extend({});
-    }).to.throw('Invalid factory');
-
-    expect(function () {
-
-        User.extend(function () {});
-    }).to.throw('Invalid factory');
-
-    done();
-});
-
-lab.test('can abort validation by returning an error in preValidate', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var called = false;
-    User.prototype.on('preValidate', function (user) {
-
-        return Promise.reject(new Error('failed'));
-    });
-
-    User.prototype.on('preValidate', function (user) {
-
-        called = true;
-    });
-
-    var user = new User({ name: 'test' });
-    user.validate().catch(function (err) {
-
-        expect(err).to.exist();
-        expect(err.message).to.equal('failed');
-        expect(user.age).to.not.exist();
-        expect(called).to.equal(false);
-        done();
-    });
-});
-
-lab.test('can pass through an error from postValidate', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    User.prototype.on('postValidate', function (user) {
-
-        return Promise.reject(new Error('failed'));
-    });
-
-    var user = new User({ name: 'test' });
-    user.validate().catch(function (err) {
-
-        expect(err).to.exist();
-        expect(err.message).to.equal('failed');
-        done();
-    });
-});
-
-lab.test('can use preValidate to populate model fields', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-        expect(model.schema.isJoi).to.equal(true);
-        model.schema = Joi.object().keys({
-            id: Joi.string().default('some_id')
-        });
-
-        model.schema = { admin: Joi.boolean().default(false) };
-
-        model.prototype.on('preValidate', function (mod) {
-
-            mod.id = 'other_id';
-        });
-    };
-
-    User.register(Plugin);
-
-    var user = new User({ name: 'test' });
-    user.validate().then(function (userModel) {
-
-        expect(userModel.id).to.equal('other_id');
-        expect(userModel.admin).to.equal(false);
-        done();
-    });
-});
-
-lab.test('can use preValidate twice', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-        expect(model.schema.isJoi).to.equal(true);
-        model.schema = Joi.object().keys({
-            id: Joi.string().default('some_id')
-        });
-
-        model.prototype.on('preValidate', function (mod) {
-
-            mod.id = 'other_id';
-        });
-
-        model.prototype.on('preValidate', function (mod) {
-
-            mod.age += 1;
-        });
-    };
-
-    User.register(Plugin);
-
-    var user = new User({ name: 'test', age: 20 });
-    user.validate().then(function () {
-
-        expect(user.age).to.equal(21);
-        expect(user.id).to.equal('other_id');
-        done();
-    });
-});
-
-lab.test('uses separate event emitters for different instances', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            id: Joi.number().integer(),
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-        expect(model.schema.isJoi).to.equal(true);
-        model.prototype.on('preValidate', function (mod) {
-
-            ++mod.id;
-        });
-    };
-
-    User.register(Plugin);
-
-    var user = new User({ id: 1, name: 'test' });
-    var user2 = new User({ id: 5, name: 'test2' });
-
-    user.validate().then(function () {
-
-        expect(user.id).to.equal(2);
-        return user2.validate();
-    }).then(function () {
-
-        expect(user2.id).to.equal(6);
-        done();
-    });
-});
-
-lab.test('fires the create event when instantiating a model', function (done) {
-
-    var User = new Factory({
-        type: 'user',
-        schema: {
-            name: Joi.string().required(),
-            age: Joi.number().integer().default(20)
-        }
-    });
-
-    var Plugin = function (model) {
-
-        expect(model).to.exist();
-        expect(model.schema.isJoi).to.equal(true);
-        model.on('create', function (mod) {
-
-            expect(mod.name).to.equal('test');
-            expect(mod.age).to.equal(20);
-            done();
-        });
-    };
-
-    User.register(Plugin);
-
-    var user = new User({ name: 'test', age: 20 });
 });
